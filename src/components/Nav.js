685d61e3302fb35ffa289725c72277878840b989
alexsmoreno <68883789+alexsmoreno@users.noreply.github.com>
2022-03-24 00:24:05 -0500

    v1

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+ const NavContenedor = styled.div`
+ padding:20px;
+
+
+ `;
+
+
+export const Nav = () => {
+  return (
+      <NavContenedor>
+    <div > Holaaaaaaaaaa  </div>
+    </NavContenedor>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {PrincipalPage} from './view/principal/PrincipalPage';
+
+const elemento = React.createElement("h1", { className: 'contenedor' }, "Hola mundo");
+const elemento2 = <h1 className='contendor'>HOLAA</h1>;
+
+/*class Primer extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { quantity: 0 }
+  }
+
+  aumentar() {
+    this.setState({ quantity: this.state.quantity + 1 })
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1 className='cotenedor'>{this.state.quantity}</h1>
+        <button onClick={() => this.aumentar()}>Aumentar</button>
+
+      </div>
+    );
+  }
+
+
+}*/
+/*
+function PrimerHuck({titulo}) {
+ const [result,setResul] =useState(1)
+ const aumentar=()=>{
+   setResul(result+1);
+ }
+
+  return(
+    <div>
+      <h1 className='cotenedor'>{titulo} {result}</h1>
+      <button onClick={aumentar} >Aumentar</button>
+    </div>
+  )
+
+
+
+}*/
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <PrincipalPage/> ,document.getElementById('root')
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
